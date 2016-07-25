f64c34613809199e9e89c72506ebc2000c39112b
Morgan Higby-Flowers <mhigbyflowers@gmail.com>
2016-07-25 15:27:50 -0500

    photos page routed

--- app/Http/routes.js
+++ app/Http/routes.js
@@ -15,6 +15,9 @@
 | Route.resource('user', 'UserController')
 */
 
-const Route = use('Route')
+const Route = use('Route');
 
-Route.on('/').render('welcome')
+Route.on('/').render('welcome');
+Route.get('/photos', function * (req, res) {
+    yield res.sendView('photos');
+});
